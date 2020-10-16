1468533651d2e24a4079487a2f36285f6fc11c5b
Anderson Misson <missondeveloper@gmail.com>
2020-10-16 17:21:00 -0300

    Aula 03-06 - Cadastrando Nova Categoria com POST

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.algaworks.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
@@ -20,5 +30,25 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return this.categoriaRepository.findById(codigo).orElse(null);
+	}
+	
+	// RequestBody serve para criar uma nova categoria, caso ainda não tenha.
+	@PostMapping
+	// @ResponseStatus(HttpStatus.CREATED) // STATUS 201: CREATED, como eu coloquei ResponseEntity, ele ja avisa o 201
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		// ServletUriComponentsBuilder é um helper do Spring
+		// fromCurrentRequestUri = pegando na posição atual de categorias, adicionaro "codigo e colocar na URI"
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva); // Com isso já sai CODIGO e o NOME na resposta
 	}
 }
