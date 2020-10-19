c9a98b298eb81da4ffd1e090bc696119ada343c7
Anderson Misson <missondeveloper@gmail.com>
2020-10-19 20:50:22 -0300

    Aula 03-09 - Tratando erros com ExceptionHandler

---
+++ src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.algaworks.algamoney.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+// Toda classe que controla os avisos de requisião, tem que tem a anotação ControllerAdvice
+@ControllerAdvice
+public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	// Para ler o MESSAGES.PROPERTIES
+	@Autowired
+	private MessageSource messageSource;
+	
+	
+	// handleHttpMessageNotReadable só escrever isso, e ele já preenche o metodo, serve como mensagem para falha de requisição
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		String menssagemUsuario = messageSource.getMessage("mensagem.invalida",null, LocaleContextHolder.getLocale());
+		
+		String mensagemDesenvolvedor = ex.getCause().toString(); // Fazer uma Classe para fazer funcionar (Erro)
+		
+		// ex ->
+		// body -> local aonde você deixa a mensagem
+		// header ->
+		// status -> HttpStatus.BAD_REQUEST
+		// request ->
+		return handleExceptionInternal(ex, new Erro(menssagemUsuario,mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	public static class Erro {
+		
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+		
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			super();
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+
+	}
+	
+	
+
+}
