21c2c287748c908b42e7e3669f2503d1b5b0f5c4
Anderson Misson <missondeveloper@gmail.com>
2020-11-09 18:11:02 -0300

    Aula 06-06 - Renovando o access token como o refresh token

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.refresh.RefreshTokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -25,8 +26,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient("angular") // nome do usuario
 			.secret("@ngul@r0") // senha
 			.scopes("read", "write") // Escopo LER e GRAVAR
-			.authorizedGrantTypes("password") // Modo de tokens para usuario, sinceramente nao entendi essa parte
-			.accessTokenValiditySeconds(1800); // Quantos segundos esse Token ficara ativo. 1800/60 = 30 min
+			.authorizedGrantTypes("password", "refresh_token") // Modo de tokens para usuario, sinceramente nao entendi essa parte
+			.accessTokenValiditySeconds(20) // Quantos segundos esse Token ficara ativo. 1800/60 = 30 min
+			.refreshTokenValiditySeconds(3600 * 24);
 	}
 	
 	@Override
@@ -34,6 +36,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.tokenStore(tokenStore()) // Aonde ele vai guardar essa string
 			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false) // Com isso, sempre teremos um novo token e não vai expirar
 			.authenticationManager(authenticationManager);
 	}
 	
