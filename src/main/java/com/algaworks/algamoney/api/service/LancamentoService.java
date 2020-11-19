ecfae6e81dfa41327ec992f58d785893eb59f959
Anderson Misson <missondeveloper@gmail.com>
2020-11-19 16:14:06 -0300

    Aula 07-09 - Desafio - Atualização de Lancamento

--- src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -2,6 +2,7 @@ package com.algaworks.algamoney.api.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +14,47 @@ import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaE
 
 @Service
 public class LancamentoService {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
 	public Lancamento salvar(Lancamento lancamento) {
-	    Optional<Pessoa> pessoaOpt = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		validarPessoa(lancamento);
 
-	    if (!pessoaOpt.isPresent() || pessoaOpt.get().isInativo()) {
+		return lancamentoRepository.save(lancamento);
+	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+	    Optional<Pessoa> pessoaOpt = null;  
+
+	    if (lancamento.getPessoa().getCodigo() != null) {
+	        pessoaOpt = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+	    }
+		
+	    if (pessoaOpt == null || lancamento.getPessoa().isInativo() || pessoaOpt.get().isInativo()) {
 	        throw new PessoaInexistenteOuInativaException();
 	    }
-	    
-	    return lancamentoRepository.save(lancamento);
 	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		 Optional<Lancamento> lancamentoSalvoOpt = lancamentoRepository.findById(codigo);
+
+		    // se o valor estiver presente, retorna o valor, senão lança uma exceção
+		    return lancamentoSalvoOpt.orElseThrow(() -> new IllegalArgumentException()); 
+		}
+
 }
