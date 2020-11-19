0a49407ca1435912b3e86d1c91d0b427327fe3ff
Anderson Misson <missondeveloper@gmail.com>
2020-11-19 13:51:02 -0300

    Aula 07-06 - Alternando OAuth2 e Basic Security com profiles

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -19,50 +20,53 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.algaworks.algamoney.api.config.token.CustomTokenEnhancer;
 
+
+
 @Configuration
+@Profile("oauth-security")
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
 	@Autowired
 	private UserDetailsService userDetailsService;
-
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory() // Está gravado em memoria, mas pode ser por JDBC (banco de dados)
-				.withClient("angular") // nome do usuario
-				.secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G") // senha
-				.scopes("read", "write") // Escopo LER e GRAVAR
-				.authorizedGrantTypes("password", "refresh_token") // Modo de tokens para usuario, sinceramente nao entendi essa parte
-				.accessTokenValiditySeconds(1800) // Quantos segundos esse Token ficara ativo. 1800/60 = 30 min
-				.refreshTokenValiditySeconds(3600 * 24)
+		clients.inMemory()
+				.withClient("angular")
+				.secret("$2a$10$wgw84qaq.XDOvcsmhBfLOe1xDc0IY.2PUojWtrURAPTeaqKmW5ybu")
+				.scopes("read","write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(1800)
+				.refreshTokenValiditySeconds(3600*24)
 			.and()
-				.withClient("mobile") // nome do usuario
-				.secret("m0b1l30") // senha
-				.scopes("read") // Escopo somente LER
-				.authorizedGrantTypes("password", "refresh_token") // Modo de tokens para usuario, sinceramente nao entendi essa parte
-				.accessTokenValiditySeconds(1800) // Quantos segundos esse Token ficara ativo. 1800/60 = 30 min
-				.refreshTokenValiditySeconds(3600 * 24);
+				.withClient("mobile")
+				.secret("$2a$10$2AxzEQmnWmtr3ma4P8.rROMH7eXC2nuLg0bHKhOB7eIViSui5JPBC")
+				.scopes("read")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(1800)
+				.refreshTokenValiditySeconds(3600*24);
 	}
 	
 	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
 		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
 		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
 		
 		endpoints
 		.tokenStore(tokenStore())
-		.tokenEnhancer(tokenEnhancerChain)
+		.accessTokenConverter(this.accessTokenConverter())
 		.reuseRefreshTokens(false)
-		.authenticationManager(authenticationManager);
+		.userDetailsService(this.userDetailsService)
+		.authenticationManager(this.authenticationManager);
 	}
-	
+
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-		accessTokenConverter.setSigningKey("algaworks");
+		accessTokenConverter.setSigningKey("AlgaWorks");
 		return accessTokenConverter;
 	}
 
@@ -73,6 +77,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Bean
 	public TokenEnhancer tokenEnhancer() {
-	    return new CustomTokenEnhancer();
+		return new CustomTokenEnhancer();
 	}
 }
--- src/main/java/com/algaworks/algamoney/api/config/OAuthSecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/OAuthSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algamoney.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,19 +10,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@Profile("oauth-security")
 @EnableWebSecurity
 public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-    	return new BCryptPasswordEncoder();
-    }
-   
-    
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
